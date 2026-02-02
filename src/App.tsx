9c056ea711b258c16be7f9830c89d1cb38c23cad
VoMinhQuan184615 <vominhqun14112004@gmail.com>
2026-02-02 13:30:24 +0700

    feat:add auth function register

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { useState } from "react";
 import LoginPage from "./features/auth/page/LoginPage";
+import SignupPage from "./features/auth/page/SignUpPage";
 import { Toaster } from "./components/ui/sonner";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isAuthenticated] = useState(false);
 
   return (
-    <>
-      <LoginPage />
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? <div>Dashboard</div> : <Navigate to="/login" />
+          }
+        />
+      </Routes>
       <Toaster />
-    </>
+    </Router>
   );
 }
 
