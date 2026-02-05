cd7b95b71ef027706960de5c4f33c7f308180e22
Nguyễn Bá Đoàn <Doannbse184608@fpt.edu.vn>
2026-02-05 07:59:21 +0700

    refactor: nginx.conf

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_BASE_URL;
 
+console.log(API_URL + "HAHAHAHA")
+
 /**
  * Axios instance for API requests with auth interceptors
  */
